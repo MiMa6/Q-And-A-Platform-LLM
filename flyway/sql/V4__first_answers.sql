353624f7efd9b0cedd9faf71d2fb34fc3f6f188f
MM <michele.makiki@gmail.com>
2024-06-18 12:10:05 +0300

    Add sample answers to premade questions

--- flyway/sql/V4__first_answers.sql
+++ flyway/sql/V4__first_answers.sql
@@ -1,3 +1,31 @@
+
+INSERT INTO answers (question_id, user_uuid, answer_text)
+VALUES (1, '123', 'Main focus of the course is to provide comprehensive training in both front-end and back-end technologies building real-world apps');
+
+INSERT INTO answers (question_id, user_uuid, answer_text)
+VALUES (1, '123', 'Good software development practices');
+
+INSERT INTO answers (question_id, user_uuid, answer_text)
+VALUES (1, '123', 'Continuous learning');
+
+INSERT INTO answers (question_id, user_uuid, answer_text)
+VALUES (2, '123', 'Learn a wide range of technologies including JavaScript framework React and Node.js');
+
+INSERT INTO answers (question_id, user_uuid, answer_text)
+VALUES (2, '123', 'Redux, Express, MongoDB');
+
+INSERT INTO answers (question_id, user_uuid, answer_text)
+VALUES (2, '123', 'Proficiency in HTML, CSS and Git');
+
+INSERT INTO answers (question_id, user_uuid, answer_text)
+VALUES (3, '123', 'Principles for developing web applications that can handle high traffic and scale effectively. The course focuses on topics such as load balancing, caching, database optimization, distributed systems, and cloud computing');
+
+INSERT INTO answers (question_id, user_uuid, answer_text)
+VALUES (3, '123', 'Principles for how cloud computing technologies can be leveraged');
+
+INSERT INTO answers (question_id, user_uuid, answer_text)
+VALUES (3, '123', 'Best practices for designing robust and resilient architectures in distributed systems');
+
 INSERT INTO answers (question_id, user_uuid, answer_text)
 VALUES (4, '769c86d3-812a-48ac-ad27-5af983265610', 'Microservices: Breaking down an application into smaller, independent services that communicate with each other');
 
